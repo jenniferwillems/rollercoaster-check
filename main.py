45fc8a6b279d71ec429851557c6318e6e2445561
jenniferwillems <jen.willems17@gmail.com>
2021-02-01 09:50:22 +0100

    Update main.py
    change the time from 2 to 1 second

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ file1.close()
 
 file2 = open("rules/height.txt", "r")
 height_check = int(file2.read())
-file1.close()
+file2.close()
 
 running = True
 while running:
@@ -25,15 +25,15 @@ while running:
     #Process checks
     if(age > age_check and height > height_check):
         os.system('cls')
-        print("Stap maar in!!")
+        print("Stap maar in!")
         print(coasterimg.get())
-        time.sleep(2)
+        time.sleep(1)
 
     else:
         os.system('cls')
         print("Je voldoet niet aan de voorwaarden...")
         print(coasterimg.sad())
-        time.sleep(2)
+        time.sleep(1)
 
     result = input("Druk op Enter om nog een keer te checken, of X om te stoppen\n\n")
     if(result.upper() == "X"):
